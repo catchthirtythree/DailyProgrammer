a2c0d0f0e5204eaf41f351b0480001aabefebd06
catchthirtythree <b0red@hotmail.com>
2015-08-24 20:01:17 -0400

    Receive input from args

--- src/challenges/java/C0229E.java
+++ src/challenges/java/C0229E.java
@@ -6,26 +6,8 @@ public class C0229E {
 	private static final double EPSILON = 0.00001;
 	
 	public static void main(String[] args) {
-		double result = 2;
+		double result = Double.parseDouble(args[0]);
 		Function<Double, Double> operation = x -> Math.cos(x);
-		while (!compare(result, result = operation.apply(result))) {}
-		System.out.println(result);
-		
-		// Optional #1
-		result = 2;
-		operation = x -> x - Math.tan(x);
-		while (!compare(result, result = operation.apply(result))) {}
-		System.out.println(result);
-		
-		// Optional #2
-		result = 2;
-		operation = x -> 1 + 1 / x;
-		while (!compare(result, result = operation.apply(result))) {}
-		System.out.println(result);
-		
-		// Optional #3
-		result = 2;
-		operation = x -> 4 * x * (1 - x);
 		while (!compare(result, result = operation.apply(result))) {}
 		System.out.println(result);
 	}
