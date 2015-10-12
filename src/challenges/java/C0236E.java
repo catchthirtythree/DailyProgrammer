d60830a7dd312fb1d11c0f0e4dc21243ea7361bc
catchthirtythree <b0red@hotmail.com>
2015-10-12 18:42:47 -0400

    Challenge #236 Easy.

---
+++ src/challenges/java/C0236E.java
@@ -0,0 +1,40 @@
+/**
+ * @author Michael
+ *
+ * https://www.reddit.com/r/dailyprogrammer/comments/3ofsyb/20151012_challenge_236_easy_random_bag_system/
+ */
+package challenges.java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class C0236E {
+	private static final int THRESHOLD = 50;
+
+	public static void main(String[] args) {
+		List<Character> 
+			bag = new ArrayList<Character>(), 
+			chars = Arrays.asList('O', 'I', 'S', 'Z', 'L', 'J', 'T');
+		IntStream
+			.iterate(0, n -> n + 1)
+			.peek(n -> {
+				Collections.shuffle(chars);
+				bag.addAll(chars);
+			}).allMatch(n -> bag.size() < THRESHOLD);
+		System.out.println(bag.subList(0, THRESHOLD));
+	}
+
+	/* Alternate way to shuffle before I realized I could put code blocks in peek() */
+	public static List<Character> shuffle(List<Character> list) {
+		return new Random()
+			.ints(0, list.size())
+			.distinct().limit(list.size())
+			.mapToObj(i -> list.get(i))
+			.collect(Collectors.toList());
+	}
+}
