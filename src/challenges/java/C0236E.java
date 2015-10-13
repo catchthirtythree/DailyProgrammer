10acce9ca47d68215e1e484705f0321825b13029
catchthirtythree <b0red@hotmail.com>
2015-10-12 21:04:43 -0400

    Problem 236 Easy verification.

--- src/challenges/java/C0236E.java
+++ src/challenges/java/C0236E.java
@@ -27,6 +27,17 @@ public class C0236E {
 				bag.addAll(chars);
 			}).allMatch(n -> bag.size() < THRESHOLD);
 		System.out.println(bag.subList(0, THRESHOLD));
+		
+		/* Verification: make sure there are no doubles. */
+		boolean valid = IntStream
+			.iterate(0, n -> n + chars.size())
+			.limit((int) (Math.ceil(bag.size() / chars.size())))
+			.allMatch(n -> bag.subList(n, n + chars.size() < THRESHOLD ? n + chars.size() : n + THRESHOLD - n)
+					.stream()
+					.distinct()
+					.count() == (n + chars.size() < THRESHOLD ? chars.size() : THRESHOLD - n));
+		
+		System.out.println(valid);
 	}
 
 	/* Alternate way to shuffle before I realized I could put code blocks in peek() */
