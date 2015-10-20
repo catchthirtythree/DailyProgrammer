a896337fd1326625075ec883f3c40f8e2deeb891
catchthirtythree <b0red@hotmail.com>
2015-10-20 02:16:20 -0400

    Validation didn't work. Made base code shorter.

--- src/challenges/java/C0236E.java
+++ src/challenges/java/C0236E.java
@@ -7,7 +7,6 @@ package challenges.java;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -21,27 +20,11 @@ public class C0236E {
 			bag = new ArrayList<Character>(), 
 			chars = Arrays.asList('O', 'I', 'S', 'Z', 'L', 'J', 'T');
 		IntStream
-			.iterate(0, n -> n + 1)
-			.peek(n -> {
-				Collections.shuffle(chars);
-				bag.addAll(chars);
-			}).allMatch(n -> bag.size() < THRESHOLD);
+			.range(0, (int) Math.ceil((double) THRESHOLD / chars.size()))
+			.forEach(n -> bag.addAll(shuffle(chars)));
 		System.out.println(bag.subList(0, THRESHOLD));
-		
-		/* Verification: make sure there are no doubles. */
-		boolean valid = IntStream
-			.iterate(0, n -> n + chars.size())
-			.limit((int) (Math.ceil(bag.size() / chars.size())))
-			.allMatch(n -> bag
-					.subList(n, n + chars.size() < THRESHOLD ? n + chars.size() : n + THRESHOLD - n)
-					.stream()
-					.distinct()
-					.count() == (n + chars.size() < THRESHOLD ? chars.size() : THRESHOLD - n));
-		
-		System.out.println(valid);
 	}
 
-	/* Alternate way to shuffle before I realized I could put code blocks in peek() */
 	public static List<Character> shuffle(List<Character> list) {
 		return new Random()
 			.ints(0, list.size())
