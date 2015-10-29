65731fef9a7b8297d8fa043fab1e4e2f14d08080
catchthirtythree <b0red@hotmail.com>
2015-10-28 20:26:48 -0400

    Forgot the comment with the link.

--- src/challenges/java/C0238I.java
+++ src/challenges/java/C0238I.java
@@ -1,3 +1,8 @@
+/**
+ * @author Michael
+ *
+ * https://www.reddit.com/r/dailyprogrammer/comments/3qjnil/20151028_challenge_238_intermediate_fallout/
+ */
 package challenges.java;
 
 import java.io.IOException;
